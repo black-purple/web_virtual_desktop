d9b21c4178d664b5a932d7a18d64078a010d1838
Mouradouchane <mouradouchane@gmail.com>
2022-12-17 19:10:53 +0100

    add:demo.mp4

--- main.js
+++ main.js
@@ -26,23 +26,21 @@ let win1 = desk.new.window(
 
 win.on.resize.all( ( w , ev ) => {
 
-    console.log( "ALLLL" , w.get.title()  , ev.clientX );
+    console.log( "resize event :" , w.get.title()  , ev.clientX );
 
 });
 
 
-let e = new element("eid","TITLE","simple text",10,10,110,120);
+let e = new element("e_id","simple text",10,10,110,120);
 
-    e.on.click(
+    e.context_menu(false);
+
+    e.on.click_right(
         ( obj , event ) => console.warn("click_down !")
     );
 
     e.on.hover( 
-        ( obj , event ) => {
-            
-            console.log("hover !" , obj.get.text() )
-
-        } 
+        ( obj , event ) => console.log("hover !" , obj.get.text() )
     );
 
 /*
